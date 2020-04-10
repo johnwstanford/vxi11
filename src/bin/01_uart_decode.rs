b9d9f4ff4e07091eabd254aaf1fd5303a677355e
John Stanford <johnwstanford@gmail.com>
2020-04-10 09:13:25 -0500

    Request sample rate after acquisition, not before

--- src/bin/01_uart_decode.rs
+++ src/bin/01_uart_decode.rs
@@ -2,7 +2,7 @@
 extern crate serde;
 extern crate vxi11;
 
-use std::io;
+use std::io::{self, Error, ErrorKind};
 
 use vxi11::devices::sds1202x::{SDS1202X, TriggerMode, protocol_decode};
 
@@ -16,27 +16,23 @@ pub fn main() -> io::Result<()> {
 	eprintln!("Initial device state");
 	eprintln!("{}", serde_json::to_string_pretty(&(sds1202x.get_full_state()?)).unwrap());
 
-	// Set up oscilloscope with state that doesn't change with frequency
-	// TODO: ensure that both channels are active
+	// Set up oscilloscope
 	sds1202x.set_voltage_div(1, 1.0)?;                            // Voltage division
 	sds1202x.set_voltage_ofs(1, 0.0)?;							  // Voltage offset
 	sds1202x.ask(b"WFSU SP,0,NP,0,FP,0")?;                        // Send all data points starting with the first one
 
-	// Acquire once before the real thing to get an accurate sample rate
-	sds1202x.arm_single()?;
-	sds1202x.force_trigger()?;
-	sds1202x.wait()?;
-
-	let samp_rate_sps:f32 = sds1202x.get_sample_rate()?;
 
 	loop {
 		// Capture the samples that count
 		sds1202x.set_trigger_mode(TriggerMode::Single)?;
 		sds1202x.wait()?;
+	
+		// Record the sample rate after acquisition
+		let samp_rate_sps:f32 = sds1202x.get_sample_rate()?;
 
 		let ch1:Vec<i8> = sds1202x.transfer_waveform_raw(1)?;
 		let uart_rx_bytes:Vec<u8> = protocol_decode::uart(&ch1, samp_rate_sps, 9600.0, 8)?;
-		let uart_rx:&str = std::str::from_utf8(&uart_rx_bytes).unwrap();
+		let uart_rx:&str = std::str::from_utf8(&uart_rx_bytes).map_err(|_| Error::new(ErrorKind::Other, "Unable to parse response as UTF-8"))?;
 		println!("{}", uart_rx);
 	}
 
